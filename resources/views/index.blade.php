fb8e4fec60646948ac9abc5065fe40cc710851c4
Becky Ada <rebtshikadila@gmail.com>
2022-04-13 18:07:07 +0200

    ajout des liens css et js

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Mon blog</title>
+<head>
+    <meta charset="utf-8">
+    <title>Mon blog</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
 
-    </head>
-    <body>
-        
-    </body>
+<body>
+
+</body>
+
 </html>
