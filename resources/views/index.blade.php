1f6eab1a2877c10423a9fa2ac71c5067d8d710bd
Becky Ada <rebtshikadila@gmail.com>
2022-04-13 15:46:56 +0200

    création de la vue index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Mon blog</title>
+
+    </head>
+    <body>
+        
+    </body>
+</html>
