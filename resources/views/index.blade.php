45ffa0c5cb96fbba7b37302f30bf69373b0c5d6f
Becky Ada <rebtshikadila@gmail.com>
2022-04-14 15:17:03 +0200

    chargement du minifier de bootstrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,8 @@
 <head>
     <meta charset="utf-8">
     <title>Mon blog</title>
-    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/main.css">
-    <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-    <script src="js/bootstrap.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
