cebe5556e5e54b773d5c0436801670fb0fb2d355
Becky Ada <rebtshikadila@gmail.com>
2022-04-14 15:30:26 +0200

    chargement du fonts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,13 +4,24 @@
 <head>
     <meta charset="utf-8">
     <title>Mon blog</title>
+    
+    {{-- font --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap" rel="stylesheet">
+    {{-- end font --}}
+
+    {{-- css --}}
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/main.css">
+    {{-- end css --}}
+
+    
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
-
+ 
 </body>
 
 </html>
