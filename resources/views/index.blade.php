9fc6e91ed03a4b64abec5c2d14689605ad17903e
Becky Ada <rebtshikadila@gmail.com>
2022-04-18 15:40:45 +0200

    ajout de la classe offset au bouton du menu

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -34,13 +34,15 @@
                 <a href="#" class="navbar-brand text-uppercase fw-bold ">
                     <span class="bg-gradient p-1 rounded-3 text-light">Becky Ada</span>
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                <div class="offset-md-7">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
 
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
                     <ul class="navbar-nav">
                         <li class="nav-item">
                             <a class="nav-link active text-white" href="#">Home</a>
